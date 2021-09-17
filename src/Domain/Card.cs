ecb97f67e6b14dc738a72324e6cdb639af426c82
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-09-17 14:37:57 +0200

    Add empty domain model

---
+++ src/Domain/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Card
+    {
+        #region Properties
+        public FaceValue FaceValue { get; }
+        public Suit Suit { get; }
+        #endregion
+
+        #region Constructors
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
